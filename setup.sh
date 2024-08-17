ecbd6e00aafd27a8db10f93160997c5fcf6038c6
NMTSolutions <nanomacrotech.soultions@gmail.com>
2024-08-17 13:28:49 +0530

    updated: local database default names and passwords

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ sudo apt-get install postgresql postgresql-contrib -y
 sudo service postgresql start
 
 # Set up PostgreSQL user and database
-sudo -u postgres psql -c "CREATE USER your_user WITH PASSWORD 'your_password';"
-sudo -u postgres psql -c "CREATE DATABASE your_db OWNER your_user;"
+sudo -u postgres psql -c "CREATE USER rpi_has WITH PASSWORD 'rpi_has';"
+sudo -u postgres psql -c "CREATE DATABASE rpi_has OWNER rpi_has;"
 
 # Install Python3 and pip
 sudo apt-get install python3 python3-pip -y
@@ -28,8 +28,11 @@ source venv/bin/activate
 # Install project dependencies
 pip install -r requirements.txt
 
-# Run migrations (assuming you use a migration tool like Alembic)
-alembic upgrade head  # or other migration commands
+#Autogenerate Migrations
+alembic revision --autogenerate -m "RPi_HAS"
 
-# Start the Python server (adjust the command based on your setup)
-python server.py  # or the entry point of your project
+# Run migrations
+alembic upgrade head
+
+# Start the Python server
+python server.py 
