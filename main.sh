44c7c9dfb1ad491b13e8c528703bc19554b57a4b
NMTSolutions <nanomacrotech.soultions@gmail.com>
2024-08-18 15:48:08 +0530

    added: remove command first in main cmd

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
 # Install Git
-sudo apt-get install git -y && git clone https://github.com/Tauqeer-Ahmed-99/RPi_HAS.git && cd ./RPi_HAS && chmod +x setup.sh && ./setup.sh
+sudo apt-get install git -y && rm -rf ./RPi_HAS && git clone https://github.com/Tauqeer-Ahmed-99/RPi_HAS.git && cd ./RPi_HAS && chmod +x setup.sh && ./setup.sh
