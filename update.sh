c8f78725e0ca3958109da98f59d417b44e8ef87e
NMTSolutions <nanomacrotech.soultions@gmail.com>
2024-09-11 18:58:13 +0530

    updated: update.sh

--- update.sh
+++ update.sh
@@ -4,17 +4,17 @@ sudo kill -9 $(lsof -t -i:8000)
 # Pull the latest code from the GitHub repository
 git pull origin master
 
-# Autogenerate migrations (new code might contain DB changes)
-alembic revision --autogenerate -m "RPi_HAS"
-
-# Run migrations
-alembic upgrade head
-
 # Use virtual environment for packages already created when setup
 source venv/bin/activate
 
 # Install project dependencies (new code might have new dependencies)
 pip install -r requirements.txt
+
+# Autogenerate migrations (new code might contain DB changes)
+alembic revision --autogenerate -m "RPi_HAS"
+
+# Run migrations
+alembic upgrade head
 
 # Start the FastAPI server
 fastapi run server.py
