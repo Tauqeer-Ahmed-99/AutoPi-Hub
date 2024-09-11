cbf0df830b049fe6dd55f04245f78862ffad66d3
NMTSolutions <nanomacrotech.soultions@gmail.com>
2024-09-11 18:24:38 +0530

    updated : update command

--- update.sh
+++ update.sh
@@ -3,17 +3,17 @@ sudo kill -9 8000
 
 git pull origin master
 
-# Use virtual environment for packages already created when setup
-source venv/bin/activate
-
-# Install project dependencies (new code might have new dependencies)
-pip install -r requirements.txt
-
 # Autogenerate Migrations (New code might contain db changes)
 alembic revision --autogenerate -m "RPi_HAS"
 
 # Run migrations (New code might contain db changes)
 alembic upgrade head
+
+# Use virtual environment for packages already created when setup
+source venv/bin/activate
+
+# Install project dependencies (new code might have new dependencies)
+pip install -r requirements.txt
 
 # Start the Python server
 fastapi run server.py
